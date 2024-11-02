109ea5c78656385f8490c8e893456a9a286c58ee
Khizar Ameer <khizarameer890@gmail.com>
2024-11-02 12:47:02 +0500

    changes in .gitignore

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['khizarameer890@gmail.com'];
+const adminEmails = ['khizarameer890@gmail.com', 'khizarameer903@gmail.com', 'junaidhussain4191@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
